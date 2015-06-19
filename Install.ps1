2b7e4355f789f6c0a30b61fe8e1b4ee96c9dde98
Randy Bell <rabell@fanatics.com>
2015-06-19 13:27:23 -0600

    Renamed IntallChocolatey to InstallChocolatey and added packages to Install.ps1.

--- Install.ps1
+++ Install.ps1
@@ -1,2 +1,19 @@
 # sublime
 cinst sublimetext3 -y
+
+# git
+cinst git -y
+# todo configure git
+
+# tortoise git
+cinst tortoisegit -y
+
+# smartgit
+cinst smartgit -y
+
+# Visual Studio
+cinst visualstudiocommunity2013
+
+# Resharper
+cinst resharper
+
